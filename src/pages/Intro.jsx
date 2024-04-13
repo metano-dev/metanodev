03f37a9cb0ba3c3299c80d2c01a4398f13b572b1
AngelSiete <andraco660@gmail.com>
2024-04-13 16:05:17 +0200

    font and intro

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -3,14 +3,15 @@ import { useNavigate } from "react-router-dom"
 
 export default function Intro () {
     const navigate = useNavigate();
-    // useEffect(()=>{
-    //     setTimeout(() => {
-    //         navigate('/home')
-    //     }, 6000)
-    // }, [])
+    useEffect(()=>{
+        setTimeout(() => {
+            navigate('/home')
+        }, 4000)
+    }, [])
     return(
-        <>
-        <p>Intro</p>
-        </>
+        <div className="m-auto md:mt-0 h-screen flex flex-col text-center justify-evenly md:justify-center">
+        <p className="text-7xl md:text-9xl">METANO - DEV</p>
+        <p className="mt-14 text-5xl md:text-7xl">Creemos en lo que hacemos</p>
+        </div>
     )
 }
